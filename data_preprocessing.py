6d8a602fbc1c6deed6ead510f1c266e435094bf2
Nandun Bandara <ntbandara3@hotmail.com>
2018-04-20 13:09:35 +0530

    Fixed issues in the algorithm

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,56 +5,53 @@ import pandas as pd
 
 np.set_printoptions(threshold=np.nan)
 
-def preprocess_data(path):
-
+def preProcessTrainingData(path):
     # Import dataset
-    dataset = pd.read_csv(path)
-    dataset.drop('Name', axis=1, inplace=True)
-    dataset.drop('Ticket', axis=1, inplace=True)
-    dataset.drop('Fare', axis=1, inplace=True)
-    dataset.drop('Embarked', axis=1, inplace=True)
+    df = pd.read_csv(path)
+    # df.drop('Name', axis=1, inplace=True)
+    # df.drop('Ticket', axis=1, inplace=True)
+    # df.drop('Fare', axis=1, inplace=True)
+    # df.drop('Embarked', axis=1, inplace=True)
+    include = ['Age', 'Sex', 'Embarked', 'Survived']
+    df_ = df[include]
 
-    # Features matrix
-    X = dataset.loc[:, ['PassengerId','Pclass','Sex','Age','SibSp','Parch','Cabin']]
+    categoricals = []
+    for col, col_type in df_.dtypes.iteritems():
+        if col_type == 'O':
+            categoricals.append(col)
+        else:
+            df_[col].fillna(0, inplace=True)
 
-    price_index = len(dataset.columns) - 1
+    df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
 
-    # Target matrix
-    Y = dataset.loc[:, 'Survived']
+    dependent_variable = 'Survived'
+    x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+    y = df_ohe[dependent_variable]
 
-    # Check if a column is categorical or not
-    def is_categorical(column):
-        return column.dtype.name == 'category' or column.dtype.name == 'object'
+    return x,y
 
-    def is_numerical(column):
-        return column.dtype.name == 'int64'
+def preProcessTestData(path):
+    # Import dataset
+    df = pd.read_csv(path)
+    # df.drop('Name', axis=1, inplace=True)
+    # df.drop('Ticket', axis=1, inplace=True)
+    # df.drop('Fare', axis=1, inplace=True)
+    # df.drop('Embarked', axis=1, inplace=True)
 
-    # Encoding categorical data
-    from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-    labelencoder_X = LabelEncoder()
+    df_ = df[['Age', 'Sex', 'Embarked']]
 
-    for column in dataset:
-        if is_categorical(dataset[column]):
-            index = dataset.columns.get_loc(column)
-            X[:, index] = labelencoder_X.fit_transform(X[:, index])
+    categoricals = []
+    for col, col_type in df_.dtypes.iteritems():
+        if col_type == 'O':
+            categoricals.append(col)
+        else:
+            df_[col].fillna(0, inplace=True)
 
-    # Handle missing/null data
-    from sklearn.preprocessing import Imputer
-    imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-    imputer = imputer.fit(X)
-    X = imputer.transform(X)
-
-    # Feature scaling
-    from sklearn.preprocessing import StandardScaler
-    ss_X = StandardScaler()
-    X = ss_X.fit_transform(X)
-    X = ss_X.transform(X)
-
-    return X,Y
+    df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
 
 
-# onehotencoder = OneHotEncoder(categorical_features=[1])
-# X[:, 1] = onehotencoder.fit_transform(X).toarray()
+    return df_ohe
+
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import data_preprocessing as dp
+import pandas as pd
 
 TRAIN_DATA_PATH = './data/train.csv'
 TEST_DATA_PATH = './data/test.csv'
 
-x_train, y_train = dp.preprocess_data(TRAIN_DATA_PATH)
+x_train, y_train = dp.preProcessTrainingData(TRAIN_DATA_PATH)
+x_test = dp.preProcessTestData(TEST_DATA_PATH)
 
-print x_train
+# using a random forest classifier (can be any classifier)
+from sklearn.ensemble import RandomForestClassifier as rf
+
+clf = rf()
+clf.fit(x_train, y_train)
+
+prediction = clf.predict(x_test)
+
+print prediction
