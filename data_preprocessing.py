982c827359932a38594c611a32598370c2fa6e1e
Nandun Bandara <ntbandara3@hotmail.com>
2018-04-28 02:49:44 +0530

    Handled NaN values

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,25 +2,32 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Imputer
 
 np.set_printoptions(threshold=np.nan)
 
 def preProcessTrainingData(path):
     # Import dataset
     dataFrame = pd.read_csv(path)
-    include = ['Age', 'Sex', 'Embarked', 'Survived']
+    include = ['Age', 'Sex', 'Embarked','SibSp', 'Parch','Cabin', 'Survived']
     dataFrame_ = dataFrame[include]
 
     categoricals = []
     for col, col_type in dataFrame_.dtypes.iteritems():
         if col_type == 'O':
             categoricals.append(col)
-        else:
-            dataFrame_[col].fillna(0, inplace=True)
+        # else:
+        #     dataFrame_[col].fillna(0, inplace=True)
 
     dataFrameEncoded = pd.get_dummies(dataFrame_, columns=categoricals, dummy_na=True)
 
+    # set the mean value of the column for nan
+    imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imputer = imputer.fit(dataFrameEncoded)
+    dataFrameEncoded[:] = imputer.transform(dataFrameEncoded[:])
+
     dependent_variable = 'Survived'
+
     x = dataFrameEncoded[dataFrameEncoded.columns.difference([dependent_variable])]
     y = dataFrameEncoded[dependent_variable]
 
