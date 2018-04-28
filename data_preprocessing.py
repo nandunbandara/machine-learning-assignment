f601d68311d2d3dcbd2c015952c3486b49a23e8a
Nandun Bandara <ntbandara3@hotmail.com>
2018-04-28 19:29:37 +0530

    implementation changed

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -10,7 +10,11 @@ np.set_printoptions(threshold=np.nan)
 def preProcessTrainingData(path):
     # Import dataset
     dataFrame = pd.read_csv(path)
-    include = ['Age', 'Sex', 'Embarked','SibSp', 'Parch','Cabin', 'Survived']
+    include = ['URL', 'URL_LENGTH', 'NUMBER_SPECIAL_CHARACTERS','CHARSET',
+               'SERVER','CONTENT_LENGTH','WHOIS_COUNTRY','WHOIS_STATEPRO',
+               'WHOIS_REGDATE','WHOIS_UPDATED_DATE','TCP_CONVERSATION_EXCHANGE',
+               'DIST_REMOTE_TCP_PORT','REMOTE_IPS','APP_BYTES','SOURCE_APP_PACKETS',
+               'REMOTE_APP_PACKETS','APP_PACKETS','DNS_QUERY_TIMES','Type']
     dataFrame_ = dataFrame[include]
 
     categoricals = []
@@ -27,42 +31,19 @@ def preProcessTrainingData(path):
     imputer = imputer.fit(dataFrameEncoded)
     dataFrameEncoded[:] = imputer.transform(dataFrameEncoded[:])
 
-    dependent_variable = 'Survived'
+    dependent_variable = 'Type'
 
     x = dataFrameEncoded[dataFrameEncoded.columns.difference([dependent_variable])]
     y = dataFrameEncoded[dependent_variable]
 
     # Scale the age variable
-    scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
-    age = x[['Age']].values.astype(int)
-    age_scaled = scaler.fit_transform(age)
-    x['Age'] = age_scaled
+    # scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
+    # age = x[['Age']].values.astype(int)
+    # age_scaled = scaler.fit_transform(age)
+    # x['Age'] = age_scaled
 
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
     return x_train, x_test, y_train, y_test
-
-def preProcessTestData(path):
-    # Import dataset
-    dataFrame = pd.read_csv(path)
-
-    dataFrame_ = dataFrame[['Age', 'Sex', 'Embarked']]
-
-    categoricals = []
-    for col, col_type in dataFrame_.dtypes.iteritems():
-        if col_type == 'O':
-            categoricals.append(col)
-        else:
-            dataFrame_[col].fillna(0, inplace=True)
-
-    dataFrameEncoded = pd.get_dummies(dataFrame_, columns=categoricals, dummy_na=True)
-
-
-    return dataFrameEncoded
-
-
-
-
-
 
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import data_preprocessing as dp
 import pandas as pd
 
-TRAIN_DATA_PATH = './data/train.csv'
-TEST_DATA_PATH = './data/test.csv'
+DATA_PATH = './data/dataset.csv'
 
-x_train, x_test, y_train, y_test = dp.preProcessTrainingData(TRAIN_DATA_PATH)
+x_train, x_test, y_train, y_test = dp.preProcessTrainingData(DATA_PATH)
 # x_test = dp.preProcessTestData(TEST_DATA_PATH)
 
 # using a random forest classifier (can be any classifier)
 from sklearn.ensemble import RandomForestClassifier as rf
 
-classifier = rf(n_estimators=100, criterion='gini', min_samples_split=1, min_samples_leaf=1, bootstrap=True, oob_score=True,
-                n_jobs=4, random_state=2, warm_start=False, class_weight=None)
+classifier = rf(n_estimators=100, criterion='gini', min_samples_leaf=1, bootstrap=True, oob_score=True,
+                n_jobs=1, random_state=2, warm_start=False, class_weight=None)
 
 classifier.fit(x_train, y_train)
 
