55de0882581ba869882740b81e89fdaafef65c50
Nandun Bandara <ntbandara3@hotmail.com>
2018-04-29 12:11:33 +0530

    Output format changes

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,14 +4,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import StandardScaler
+import datetime as dt
 
 np.set_printoptions(threshold=np.nan)
 
 def preProcessTrainingData(path):
     # Import dataset
     dataFrame = pd.read_csv(path)
-    include = ['URL', 'URL_LENGTH', 'NUMBER_SPECIAL_CHARACTERS','CHARSET',
-               'SERVER','CONTENT_LENGTH','WHOIS_COUNTRY','WHOIS_STATEPRO',
+    include = ['URL_LENGTH', 'NUMBER_SPECIAL_CHARACTERS',
+               'CONTENT_LENGTH','WHOIS_COUNTRY','WHOIS_STATEPRO',
                'WHOIS_REGDATE','WHOIS_UPDATED_DATE','TCP_CONVERSATION_EXCHANGE',
                'DIST_REMOTE_TCP_PORT','REMOTE_IPS','APP_BYTES','SOURCE_APP_PACKETS',
                'REMOTE_APP_PACKETS','APP_PACKETS','DNS_QUERY_TIMES','Type']
@@ -21,8 +22,20 @@ def preProcessTrainingData(path):
     for col, col_type in dataFrame_.dtypes.iteritems():
         if col_type == 'O':
             categoricals.append(col)
-        # else:
-        #     dataFrame_[col].fillna(0, inplace=True)
+        else:
+            dataFrame_[col].fillna(0, inplace=True)
+
+    # dataFrame_['WHOIS_REGDATE'] = np.where(dataFrame_['WHOIS_REGDATE'] == 'None','19/03/2017 0:00', dataFrame_['WHOIS_REGDATE'])
+    # dataFrame_['WHOIS_UPDATED_DATE'] = np.where(dataFrame_['WHOIS_UPDATED_DATE'] == 'None', '19/03/2017 0:00',
+    #                                        dataFrame_['WHOIS_UPDATED_DATE'])
+    #
+    # dataFrame_['WHOIS_REGDATE'] = pd.to_datetime(dataFrame_['WHOIS_REGDATE'])
+    # dataFrame_['WHOIS_REGDATE'] = dataFrame_['WHOIS_REGDATE'].map(dt.datetime.toordinal)
+    #
+    # dataFrame_['WHOIS_UPDATED_DATE'] = pd.to_datetime(dataFrame_['WHOIS_UPDATED_DATE'])
+    # dataFrame_['WHOIS_UPDATED_DATE'] = dataFrame_['WHOIS_UPDATED_DATE'].map(dt.datetime.toordinal)
+    #
+    # print pd.to_datetime('None')
 
     dataFrameEncoded = pd.get_dummies(dataFrame_, columns=categoricals, dummy_na=True)
 
@@ -43,7 +56,17 @@ def preProcessTrainingData(path):
     # x['Age'] = age_scaled
 
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+    print "Training Dataset"
+    print "................"
+    print "Benign: %d" % sum(float(num) == 0 for num in y_train)
+    print "Malicious: %d" % sum(float(num) == 1 for num in y_train)
+
+    print "\nTest Dataset"
+    print "................"
+    print "Benign: %d" % sum(float(num) == 0 for num in y_test)
+    print "Malicious: %d" % sum(float(num) == 1 for num in y_test)
 
     return x_train, x_test, y_train, y_test
 
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import data_preprocessing as dp
-import pandas as pd
+from sklearn.metrics import accuracy_score, mean_squared_error
+
 
 DATA_PATH = './data/dataset.csv'
 
 x_train, x_test, y_train, y_test = dp.preProcessTrainingData(DATA_PATH)
-# x_test = dp.preProcessTestData(TEST_DATA_PATH)
 
-# using a random forest classifier (can be any classifier)
+# using a random forest classifier
 from sklearn.ensemble import RandomForestClassifier as rf
 
 classifier = rf(n_estimators=100, criterion='gini', min_samples_leaf=1, bootstrap=True, oob_score=True,
@@ -14,9 +14,11 @@ classifier = rf(n_estimators=100, criterion='gini', min_samples_leaf=1, bootstra
 
 classifier.fit(x_train, y_train)
 
+print "\nPredicting..."
 prediction = classifier.predict(x_test)
 
-from sklearn.metrics import accuracy_score
-
+print "\n\nPrediction"
+print ".........."
 print "Number of correct predictions: %d/%d" % (accuracy_score(y_test, prediction, normalize=False), y_test.size)
 print "Accuracy: %f" % (accuracy_score(y_test, prediction)*100)
+print "Mean Squared Error: %f" % mean_squared_error(y_test, prediction)
