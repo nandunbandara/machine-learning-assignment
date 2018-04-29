56ec7c1f3e1c15e8b6deed03db23047080555cd1
Nandun Bandara <ntbandara3@hotmail.com>
2018-04-29 17:59:34 +0530

    Date values handled

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,57 +3,69 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 import datetime as dt
+from dateutil.parser import parse
+
 
 np.set_printoptions(threshold=np.nan)
 
+def is_date(string):
+    try:
+        parse(string)
+        return True
+    except ValueError:
+        return False
+
 def preProcessTrainingData(path):
     # Import dataset
-    dataFrame = pd.read_csv(path)
+    dataframe = pd.read_csv(path)
     include = ['URL_LENGTH', 'NUMBER_SPECIAL_CHARACTERS',
                'CONTENT_LENGTH','WHOIS_COUNTRY','WHOIS_STATEPRO',
                'WHOIS_REGDATE','WHOIS_UPDATED_DATE','TCP_CONVERSATION_EXCHANGE',
                'DIST_REMOTE_TCP_PORT','REMOTE_IPS','APP_BYTES','SOURCE_APP_PACKETS',
                'REMOTE_APP_PACKETS','APP_PACKETS','DNS_QUERY_TIMES','Type']
-    dataFrame_ = dataFrame[include]
+    dataframe_ = dataframe[include]
 
-    categoricals = []
-    for col, col_type in dataFrame_.dtypes.iteritems():
+    for i,v in dataframe_['WHOIS_REGDATE'].items():
+        if not is_date(v):
+            dataframe_.loc[i,['WHOIS_REGDATE']] = '19/03/2017 0:00'
+
+    for i, v in dataframe_['WHOIS_UPDATED_DATE'].items():
+        if not is_date(v):
+            dataframe_.loc[i,['WHOIS_UPDATED_DATE']] = '19/03/2017 0:00'
+
+    try :
+
+        dataframe_.loc[:,'WHOIS_REGDATE'] = pd.to_datetime(dataframe_['WHOIS_REGDATE'])
+        dataframe_.loc[:,'WHOIS_REGDATE'] = dataframe_['WHOIS_REGDATE'].map(dt.datetime.toordinal)
+
+        dataframe_.loc[:,'WHOIS_UPDATED_DATE'] = pd.to_datetime(dataframe_['WHOIS_UPDATED_DATE'])
+        dataframe_.loc[:,'WHOIS_UPDATED_DATE'] = dataframe_['WHOIS_UPDATED_DATE'].map(dt.datetime.toordinal)
+
+    except ValueError:
+        print "ERROR"
+
+    categorical_columns = []
+    for col, col_type in dataframe_.dtypes.iteritems():
         if col_type == 'O':
-            categoricals.append(col)
-        else:
-            dataFrame_[col].fillna(0, inplace=True)
+            categorical_columns.append(col)
 
-    # dataFrame_['WHOIS_REGDATE'] = np.where(dataFrame_['WHOIS_REGDATE'] == 'None','19/03/2017 0:00', dataFrame_['WHOIS_REGDATE'])
-    # dataFrame_['WHOIS_UPDATED_DATE'] = np.where(dataFrame_['WHOIS_UPDATED_DATE'] == 'None', '19/03/2017 0:00',
-    #                                        dataFrame_['WHOIS_UPDATED_DATE'])
-    #
-    # dataFrame_['WHOIS_REGDATE'] = pd.to_datetime(dataFrame_['WHOIS_REGDATE'])
-    # dataFrame_['WHOIS_REGDATE'] = dataFrame_['WHOIS_REGDATE'].map(dt.datetime.toordinal)
-    #
-    # dataFrame_['WHOIS_UPDATED_DATE'] = pd.to_datetime(dataFrame_['WHOIS_UPDATED_DATE'])
-    # dataFrame_['WHOIS_UPDATED_DATE'] = dataFrame_['WHOIS_UPDATED_DATE'].map(dt.datetime.toordinal)
-    #
-    # print pd.to_datetime('None')
-
-    dataFrameEncoded = pd.get_dummies(dataFrame_, columns=categoricals, dummy_na=True)
+    dataframe_encoded = pd.get_dummies(dataframe_, columns=categorical_columns, dummy_na=True)
 
     # set the mean value of the column for nan
-    imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-    imputer = imputer.fit(dataFrameEncoded)
-    dataFrameEncoded[:] = imputer.transform(dataFrameEncoded[:])
+    impute = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    impute = impute.fit(dataframe_encoded)
+    dataframe_encoded[:] = impute.transform(dataframe_encoded[:])
 
     dependent_variable = 'Type'
 
-    x = dataFrameEncoded[dataFrameEncoded.columns.difference([dependent_variable])]
-    y = dataFrameEncoded[dependent_variable]
+    x = dataframe_encoded[dataframe_encoded.columns.difference([dependent_variable])]
+    y = dataframe_encoded[dependent_variable]
 
-    # Scale the age variable
-    # scaler = StandardScaler(copy=True, with_mean=True, with_std=True)
-    # age = x[['Age']].values.astype(int)
-    # age_scaled = scaler.fit_transform(age)
-    # x['Age'] = age_scaled
+    # standardize the data set
+    scale = MinMaxScaler()
+    x = scale.fit_transform(x)
 
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
